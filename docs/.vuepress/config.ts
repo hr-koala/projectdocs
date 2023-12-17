9bd8f211ff9303ca31f5f6c7c3ac0d0a10ef60ec
LAPTOP-HGA8FCV6\htong <htong890@outlook.com>
2023-12-17 13:12:46 +0800

    fix: config;

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,7 +13,7 @@ import {
 } from "./configs/index.ts";
 
 export default defineUserConfig({
-  base: "/projectdocs/",
+  base: "/projectdocs.github.io/",
   host: "0.0.0.0",
   locales: {
     "/": {
